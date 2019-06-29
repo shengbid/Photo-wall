376f466b15bd50060a5a13e2b3413dc4c574b5ca
laism@g2l-service.com <laism@g21-service.com>
2019-06-29 17:13:17 +0800

    first commit

---
+++ img.js
@@ -0,0 +1,45 @@
+
+  var list = [
+    {
+      "id": 0,
+      "name": "1",
+      "date": "2017-5-26",
+      "url": "http://img4.imgtn.bdimg.com/it/u=1433191751,1566568157&fm=26&gp=0.jpg"
+    },
+    {
+      "id": 1,
+      "name": "1",
+      "date": "2017-5-26",
+      "url": "http://img0.imgtn.bdimg.com/it/u=3942894418,502646244&fm=26&gp=0.jpg"
+    },
+    {
+      "id": 2,
+      "name": "1",
+      "date": "2017-5-26",
+      "url": "http://img2.imgtn.bdimg.com/it/u=2968413850,4223549306&fm=26&gp=0.jpg"
+    },
+    {
+      "id": 3,
+      "name": "1",
+      "date": "2017-5-26",
+      "url": "http://img2.imgtn.bdimg.com/it/u=2535548369,3399272173&fm=26&gp=0.jpg"
+    },
+    {
+      "id": 4,
+      "name": "1",
+      "date": "2017-5-26",
+      "url": "http://img4.imgtn.bdimg.com/it/u=2434347346,2305367638&fm=26&gp=0.jpg"
+    },
+    {
+      "id": 5,
+      "name": "1",
+      "date": "2017-5-26",
+      "url": "http://img5.imgtn.bdimg.com/it/u=1634273909,305714040&fm=26&gp=0.jpg"
+    },
+    {
+      "id": 6,
+      "name": "1",
+      "date": "2017-5-26",
+      "url": "http://img1.imgtn.bdimg.com/it/u=2445567090,1948244513&fm=26&gp=0.jpg"
+    }
+  ]
